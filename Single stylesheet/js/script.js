5395e94438eaca91ac99b1d44943b4269befa4e8
Mathias Bynens <mathias@qiwi.be>
2011-06-06 11:14:35 +0200

    Update placeholder plugin to the latest version (1.8.3). Switch to the iOS viewport bug fix that doesn’t rely on UA sniffing.

--- Single stylesheet/js/script.js
+++ Single stylesheet/js/script.js
@@ -1,9 +1,21 @@
-if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
-if (viewportmeta) {
-viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
-document.body.addEventListener('gesturestart', function() {
-viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
-}, false);
-}
-}
+// https://gist.github.com/901295
+(function(doc) {
+
+	var addEvent = 'addEventListener',
+	    type = 'gesturestart',
+	    qsa = 'querySelectorAll',
+	    scales = [1, 1],
+	    meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+
+	function fix() {
+		meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+		doc.removeEventListener(type, fix, true);
+	}
+
+	if ((meta = meta[meta.length - 1]) && addEvent in doc) {
+		fix();
+		scales = [.25, 1.6];
+		doc[addEvent](type, fix, true);
+	}
+
+}(document));
