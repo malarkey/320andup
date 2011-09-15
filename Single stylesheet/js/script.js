80399bdcb77df7a944b9eae422ebabca629af456
Andy Clarke <malarkey@malarkey.co.uk>
2011-09-15 15:24:05 +0100

    320 and Up version 2 (alpha)

---
+++ Single stylesheet/js/script.js
@@ -0,0 +1,28 @@
+/* Author:
+
+*/
+
+
+$(document).ready(function(){
+	
+	// Run Matt Kersley's jQuery Responsive menu plugin (see plugins.js)
+	if ($.fn.mobileMenu) {
+		$('ol#id').mobileMenu({
+			switchWidth: 768,                   // width (in px to switch at)
+			topOptionText: 'Choose a page',     // first option text
+			indentString: '&nbsp;&nbsp;&nbsp;'  // string for indenting nested items
+		});
+	}
+
+	// Run Mathias Bynens jQuery placeholder plugin (see plugins.js)
+	if ($.fn.placeholder) {
+		$('input, textarea').placeholder();		
+	}
+});
+
+
+
+
+
+
+
