cc483d868cd9ee9c3e433b45dd81506a4a2a9f94
Andy Clarke <malarkey@malarkey.co.uk>
2011-09-25 15:41:32 +0100

    Added updated plugins.js files by Keith Clarke.

--- Single stylesheet/js/plugins.js
+++ Single stylesheet/js/plugins.js
@@ -1,114 +1,36 @@
-// Plugins
 
 // usage: log('inside coolFunc', this, arguments);
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
 log.history = log.history || [];   // store logs to an array for reference
 log.history.push(arguments);
-arguments.callee = arguments.callee.caller; 
-if(this.console) console.log( Array.prototype.slice.call(arguments) );
+if(this.console) {
+arguments.callee = arguments.callee.caller;
+console.log( Array.prototype.slice.call(arguments) );
+}
 };
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
 
-// make it safe to use console.log always
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info, log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
 
-// jQuery/helper plugins
-
-/*!
- * HTML5 Placeholder jQuery Plugin v1.8.2
- * @link http://github.com/mathiasbynens/Placeholder-jQuery-Plugin
- * @author Mathias Bynens <http://mathiasbynens.be/>
+/*
+ * jQuery Responsive menu plugin by Matt Kersley
+ * Converts menus into a select elements for mobile devices and low browser widths
+ * github.com/mattkersley/Responsive-Menu
  */
- 
-;(function($) {
+(function(b){var c=0;b.fn.mobileMenu=function(g){function f(a){return a.attr("id")?b("#mobileMenu_"+a.attr("id")).length>0:(c++,a.attr("id","mm"+c),b("#mobileMenu_mm"+c).length>0)}function h(a){a.hide();b("#mobileMenu_"+a.attr("id")).show()}function k(a){if(a.is("ul, ol")){var e='<select id="mobileMenu_'+a.attr("id")+'" class="mobileMenu">';e+='<option value="">'+d.topOptionText+"</option>";a.find("li").each(function(){var a="",c=b(this).parents("ul, ol").length;for(i=1;i<c;i++)a+=d.indentString;
+c=b(this).find("a:first-child").attr("href");a+=b(this).clone().children("ul, ol").remove().end().text();e+='<option value="'+c+'">'+a+"</option>"});e+="</select>";a.parent().append(e);b("#mobileMenu_"+a.attr("id")).change(function(){var a=b(this);if(a.val()!==null)document.location.href=a.val()});h(a)}else alert("mobileMenu will only work with UL or OL elements!")}function j(a){b(window).width()<d.switchWidth&&!f(a)?k(a):b(window).width()<d.switchWidth&&f(a)?h(a):!(b(window).width()<d.switchWidth)&&
+f(a)&&(a.show(),b("#mobileMenu_"+a.attr("id")).hide())}var d={switchWidth:768,topOptionText:"Select a page",indentString:"&nbsp;&nbsp;&nbsp;"};return this.each(function(){g&&b.extend(d,g);var a=b(this);b(window).resize(function(){j(a)});j(a)})}})(jQuery);
 
-	var isInputSupported = 'placeholder' in document.createElement('input'),
-	    isTextareaSupported = 'placeholder' in document.createElement('textarea');
-	if (isInputSupported && isTextareaSupported) {
-		$.fn.placeholder = function() {
-			return this;
-		};
-		$.fn.placeholder.input = $.fn.placeholder.textarea = true;
-	} else {
-		$.fn.placeholder = function() {
-			return this.filter((isInputSupported ? 'textarea' : ':input') + '[placeholder]')
-				.bind('focus.placeholder', clearPlaceholder)
-				.bind('blur.placeholder', setPlaceholder)
-			.trigger('blur.placeholder').end();
-		};
-		$.fn.placeholder.input = isInputSupported;
-		$.fn.placeholder.textarea = isTextareaSupported;
-	}
 
-	function args(elem) {
-		// Return an object of element attributes
-		var newAttrs = {},
-		    rinlinejQuery = /^jQuery\d+$/;
-		$.each(elem.attributes, function(i, attr) {
-			if (attr.specified && !rinlinejQuery.test(attr.name)) {
-				newAttrs[attr.name] = attr.value;
-			}
-		});
-		return newAttrs;
-	}
 
-	function clearPlaceholder() {
-		var $input = $(this);
-		if ($input.val() === $input.attr('placeholder') && $input.hasClass('placeholder')) {
-			if ($input.data('placeholder-password')) {
-				$input.hide().next().attr('id', $input.removeAttr('id').data('placeholder-id')).show().focus();
-			} else {
-				$input.val('').removeClass('placeholder');
-			}
-		}
-	}
+/*
+ * jQuery Extended Selectors plugin. (c) Keith Clark freely distributable under the terms of the MIT license.
+ * Adds missing -of-type pseudo-class selectors to jQuery 
+ * github.com/keithclark/JQuery-Extended-Selectors  -  twitter.com/keithclarkcouk  -  keithclark.co.uk
+ */
+(function(g){function e(a,b){for(var c=a,d=0;a=a[b];)c.tagName==a.tagName&&d++;return d}function h(a,b,c){a=e(a,c);if(b=="odd"||b=="even")c=2,a-=b!="odd";else{var d=b.indexOf("n");d>-1?(c=parseInt(b,10)||parseInt(b.substring(0,d)+"1",10),a-=(parseInt(b.substring(d+1),10)||0)-1):(c=a+1,a-=parseInt(b,10)-1)}return(c<0?a<=0:a>=0)&&a%c==0}var f={"first-of-type":function(a){return e(a,"previousSibling")==0},"last-of-type":function(a){return e(a,"nextSibling")==0},"only-of-type":function(a){return f["first-of-type"](a)&&
+f["last-of-type"](a)},"nth-of-type":function(a,b,c){return h(a,c[3],"previousSibling")},"nth-last-of-type":function(a,b,c){return h(a,c[3],"nextSibling")}};g.extend(g.expr[":"],f)})(jQuery);
 
-	function setPlaceholder(elem) {
-		var $replacement,
-		    $input = $(this),
-		    $origInput = $input,
-		    id = this.id;
-		if ($input.val() === '') {
-			if ($input.is(':password')) {
-				if (!$input.data('placeholder-textinput')) {
-					try {
-						$replacement = $input.clone().attr({ type: 'text' });
-					} catch(e) {
-						$replacement = $('<input>').attr($.extend(args(this), { type: 'text' }));
-					}
-					$replacement
-						.removeAttr('name')
-						// We could just use the `.data(obj)` syntax here, but that wouldn’t work in pre-1.4.3 jQueries
-						.data('placeholder-password', true)
-						.data('placeholder-id', id)
-						.bind('focus.placeholder', clearPlaceholder);
-					$input
-						.data('placeholder-textinput', $replacement)
-						.data('placeholder-id', id)
-						.before($replacement);
-				}
-				$input = $input.removeAttr('id').hide().prev().attr('id', id).show();
-			}
-			$input.addClass('placeholder').val($input.attr('placeholder'));
-		} else {
-			$input.removeClass('placeholder');
-		}
-	}
 
-	$(function() {
-		// Look for forms
-		$('form').bind('submit.placeholder', function() {
-			// Clear the placeholder values so they don’t get submitted
-			var $inputs = $('.placeholder', this).each(clearPlaceholder);
-			setTimeout(function() {
-				$inputs.each(setPlaceholder);
-			}, 10);
-		});
-	});
 
-	// Clear placeholder values upon page reload
-	$(window).bind('unload.placeholder', function() {
-		$('.placeholder').val('');
-	});
-
-}(jQuery));
+/*! http://mths.be/placeholder v1.8.5 by @mathias */
+(function(g,a,$){var f='placeholder' in a.createElement('input'),b='placeholder' in a.createElement('textarea');if(f&&b){$.fn.placeholder=function(){return this};$.fn.placeholder.input=$.fn.placeholder.textarea=true}else{$.fn.placeholder=function(){return this.filter((f?'textarea':':input')+'[placeholder]').bind('focus.placeholder',c).bind('blur.placeholder',e).trigger('blur.placeholder').end()};$.fn.placeholder.input=f;$.fn.placeholder.textarea=b;$(function(){$('form').bind('submit.placeholder',function(){var h=$('.placeholder',this).each(c);setTimeout(function(){h.each(e)},10)})});$(g).bind('unload.placeholder',function(){$('.placeholder').val('')})}function d(i){var h={},j=/^jQuery\d+$/;$.each(i.attributes,function(l,k){if(k.specified&&!j.test(k.name)){h[k.name]=k.value}});return h}function c(){var h=$(this);if(h.val()===h.attr('placeholder')&&h.hasClass('placeholder')){if(h.data('placeholder-password')){h.hide().next().show().focus().attr('id',h.removeAttr('id').data('placeholder-id'))}else{h.val('').removeClass('placeholder')}}}function e(){var l,k=$(this),h=k,j=this.id;if(k.val()===''){if(k.is(':password')){if(!k.data('placeholder-textinput')){try{l=k.clone().attr({type:'text'})}catch(i){l=$('<input>').attr($.extend(d(this),{type:'text'}))}l.removeAttr('name').data('placeholder-password',true).data('placeholder-id',j).bind('focus.placeholder',c);k.data('placeholder-textinput',l).data('placeholder-id',j).before(l)}k=k.removeAttr('id').hide().prev().attr('id',j).show()}k.addClass('placeholder').val(k.attr('placeholder'))}else{k.removeClass('placeholder')}}}(this,document,jQuery));
